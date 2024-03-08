c548fe8341652bd2c3c2dbcb234d603552a2e1b7
Santiagospitia <ospitia2002@gmail.com>
2024-03-08 17:39:05 -0500

    info from firestore

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
     <v-app>
         <v-app-bar>
             <v-app-bar-title>ChamanGPT</v-app-bar-title>
+            <v-btn @click="navigateToChat">
+                Chat
+            </v-btn>                        
+            <v-btn @click="navigateToFavorites">
+                Favoritos
+            </v-btn>
             <v-btn @click="toggleTheme" 
             :icon="theme.global.current.value.dark ? 
             'mdi-weather-night' : 'mdi-weather-sunny'">
@@ -16,6 +22,7 @@
 
 import {useTheme} from 'vuetify'
 import {RouterView} from 'vue-router'
+import router from './router/index.js'
 
 export default {
     components: {
@@ -28,7 +35,19 @@ export default {
             theme.global.name.value = theme.global.current.value.dark ?
              'light' : 'dark' 
         }
-        return {toggleTheme, theme}
+
+        const navigateToFavorites = () => {
+            // Inserta aquí el código para navegar a la ruta '/favoritos'
+            // Ejemplo:
+            router.push('/favoritos')
+        }
+
+        const navigateToChat = () => {
+            // Inserta aquí el código para navegar a la ruta '/favoritos'
+            // Ejemplo:
+            router.push('/chat')
+        }
+        return {toggleTheme, theme, navigateToFavorites, navigateToChat}
     },
 }
 </script>
