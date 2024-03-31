d76d7831244ea703d35397fb57ee8755d97964ba
Santiagospitia <ospitia2002@gmail.com>
2024-03-31 16:15:31 -0500

    microphone v3

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -27,6 +27,13 @@
           class="align-self-center"
         ></v-btn>
       </div>
+      <v-btn
+        @click="startRecording((text) => {
+          message += text
+        } )"
+      >
+        Micrófono
+      </v-btn>
     </v-sheet>
   </div>
 </template>
@@ -34,6 +41,12 @@
 <script>
 import Message from './Message.vue'
 import { reactive, ref } from 'vue'
+import { TranscribeStreamingClient, StartStreamTranscriptionCommand } from "@aws-sdk/client-transcribe-streaming";
+import MicrophoneStream from "microphone-stream";
+import Buffer from "buffer";
+
+// UPDATE THIS ACCORDING TO YOUR AWS CREDENTIALS:
+import { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from "../../aws";
 
 export default {
   components: {
@@ -42,6 +55,10 @@ export default {
   setup() {
     const message = ref(null);
     const messages = reactive([]);
+    let microphoneStream = undefined;
+    const language = "es-ES";
+    const SAMPLE_RATE = 44100;
+    let transcribeClient = undefined;
 
     const userInput = async () => {
       messages.push({
@@ -81,7 +98,92 @@ export default {
       message.value = null;
     }
 
-    return { userInput, message, messages };
+    const createMicrophoneStream = async () => {
+      microphoneStream = new MicrophoneStream();
+      microphoneStream.setStream(
+        await window.navigator.mediaDevices.getUserMedia({
+          video: false,
+          audio: true,
+        })
+      );
+    };
+
+    const createTranscribeClient = () => {
+      transcribeClient = new TranscribeStreamingClient({
+        region: AWS_REGION,
+        credentials: {
+          accessKeyId: AWS_ACCESS_KEY_ID,
+          secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        },
+      });
+    };
+
+    const encodePCMChunk = (chunk) => {
+      const input = MicrophoneStream.toRaw(chunk);
+      let output = new Uint8Array(input.length * 2);
+      let outputOffset = 0;
+      for (let i = 0; i < input.length; i++, outputOffset += 2) {
+        let s = Math.max(-1, Math.min(1, input[i]));
+        let val = s < 0 ? s * 0x8000 : s * 0x7fff;
+        output[outputOffset] = val & 0xff;
+        output[outputOffset + 1] = (val >> 8) & 0xff;
+      }
+      return output;
+    };  
+
+    const getAudioStream = async function* () {
+      for await (const chunk of microphoneStream) {
+        if (chunk.length <= SAMPLE_RATE) {
+          yield {
+            AudioEvent: {
+              AudioChunk: encodePCMChunk(chunk),
+            },
+          };
+        }
+      }
+    };
+
+    const startStreaming = async (language, callback) => {
+      const command = new StartStreamTranscriptionCommand({
+        LanguageCode: language,
+        MediaEncoding: "pcm",
+        MediaSampleRateHertz: SAMPLE_RATE,
+        AudioStream: getAudioStream(),
+      });
+      const data = await transcribeClient.send(command);
+      for await (const event of data.TranscriptResultStream) {
+        const results = event.TranscriptEvent.Transcript.Results;
+        if (results.length && !results[0]?.IsPartial) {
+          const newTranscript = results[0].Alternatives[0].Transcript;
+          console.log(newTranscript);
+          callback(newTranscript + " ");
+        }
+      }
+    };
+
+    const startRecording = async (callback) => {
+      if (!AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+        alert("Set AWS env variables first.");
+        return false;
+      }
+
+      if (microphoneStream || transcribeClient) {
+        stopRecording();
+      }
+      createTranscribeClient();
+      createMicrophoneStream();
+      await startStreaming(language, callback);
+    };
+
+    const stopRecording = function () {
+      if (microphoneStream) {
+        microphoneStream.stop();
+        microphoneStream.destroy();
+        microphoneStream = undefined;
+      }
+    };
+
+    return { userInput, createMicrophoneStream, createTranscribeClient, encodePCMChunk, getAudioStream, startStreaming, startRecording, stopRecording, message, messages, microphoneStream, language, SAMPLE_RATE, transcribeClient };
   }
 }
 </script>
